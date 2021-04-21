5c7faf7fd6b3137b9bc38cd03fe5b31c8113532f
John Law <johnlaw.po@gmail.com>
2021-04-21 14:42:54 +0800

    add docstrings

--- utils/extractor.py
+++ utils/extractor.py
@@ -5,7 +5,40 @@ import pandas as pd
 
 
 class Extractor:
-    def merge_cities(self, input_loc, output_loc):
+
+    def __init__(self, folder: str = "JHU-CSSE-time-series"):
+        self.confirmed_loc = os.path.join(os.path.dirname(__file__),
+                                          "..",
+                                          folder,
+                                          "csse_covid_19_data/"
+                                          "csse_covid_19_time_series/"
+                                          "time_series_covid19_confirmed_global.csv")
+        self.death_loc = os.path.join(os.path.dirname(__file__),
+                                      "..",
+                                      folder,
+                                      "csse_covid_19_data/"
+                                      "csse_covid_19_time_series/"
+                                      "time_series_covid19_deaths_global.csv")
+        self.recovered_loc = os.path.join(os.path.dirname(__file__),
+                                          "..",
+                                          folder,
+                                          "csse_covid_19_data/"
+                                          "csse_covid_19_time_series/"
+                                          "time_series_covid19_recovered_global.csv")
+        self.confirmed = None
+        self.recovered = None
+        self.dead = None
+        self.skip_list = ["Hong Kong", "Beijing"]
+
+    def merge_cities(self, input_loc: str, output_loc: str):
+        """Merge cities into countries.
+
+        :param input_loc: CSV location to read from.
+        :type input_loc: str
+        ...
+        :param output_loc: CSV location to output to.
+        :type output_loc: str
+        """
         with open(input_loc, "r") as read_obj, open(output_loc, 'w+', newline='') as write_obj:
             csv_reader = csv.reader(read_obj)
             csv_writer = csv.writer(write_obj)
@@ -31,52 +64,53 @@ class Extractor:
                     else:
                         csv_writer.writerow(lines[i])
 
-    def __init__(self, folder="JHU-CSSE-time-series"):
-        self.confirmed_loc = os.path.join(os.path.dirname(__file__),
-                                          "..",
-                                          folder,
-                                          "csse_covid_19_data/"
-                                          "csse_covid_19_time_series/"
-                                          "time_series_covid19_confirmed_global.csv")
-        self.death_loc = os.path.join(os.path.dirname(__file__),
-                                      "..",
-                                      folder,
-                                      "csse_covid_19_data/"
-                                      "csse_covid_19_time_series/"
-                                      "time_series_covid19_deaths_global.csv")
-        self.recovered_loc = os.path.join(os.path.dirname(__file__),
-                                          "..",
-                                          folder,
-                                          "csse_covid_19_data/"
-                                          "csse_covid_19_time_series/"
-                                          "time_series_covid19_recovered_global.csv")
-        self.confirmed = None
-        self.recovered = None
-        self.dead = None
-        self.skip_list = ["Hong Kong", "Beijing"]
-
     def merge(self):
         self.merge_cities(self.confirmed_loc, "confirmed.csv")
         self.merge_cities(self.death_loc, "deaths.csv")
         self.merge_cities(self.recovered_loc, "recovered.csv")
 
-    def get_confirmed(self, start_date, country):
+    def get_confirmed(self, start_date: str, country: str):
+        """Get confirmed time series data from a start date of a country.
+
+        :param start_date: The state date.
+        :type start_date: str
+        ...
+        :param country: The country name.
+        :type country: str
+        """
         df = pd.read_csv('confirmed.csv')
         country_df = df[df['Country/Region'] == country].iloc[0].loc[start_date:]
         country_df.index = pd.to_datetime(country_df.index)
         return country_df
 
-    def get_recovered(self, start_date, country):
+    def get_recovered(self, start_date: str, country: str):
+        """Get recovered time series data from a start date of a country.
+
+        :param start_date: The state date.
+        :type start_date: str
+        ...
+        :param country: The country name.
+        :type country: str
+        """
         df = pd.read_csv('recovered.csv')
         country_df = df[df['Country/Region'] == country].iloc[0].loc[start_date:]
         country_df.index = pd.to_datetime(country_df.index)
         return country_df
 
-    def get_dead(self, start_date, country):
+    def get_dead(self, start_date: str, country: str):
+        """Get dead time series data from a start date of a country.
+
+        :param start_date: The state date.
+        :type start_date: str
+        ...
+        :param country: The country name.
+        :type country: str
+        """
         df = pd.read_csv('deaths.csv')
         country_df = df[df['Country/Region'] == country].iloc[0].loc[start_date:]
         country_df.index = pd.to_datetime(country_df.index)
         return country_df
+
 
 if __name__ == "__main__":
     extractor = Extractor()
